6f1fabc27836afa53b685275b4c40637699a6c5a
fattatihafssa <fattatihafssa@gmail.com>
2026-09-24 10:41:27 +0000

    eee

--- MINI PROJECT .js
+++ MINI PROJECT .js
@@ -7,5 +7,24 @@ let menu = [
   for(let i = 0 ; i <menu.length ; i++)
  
     console.log(menu[i].name  ,  menu [i].price ,"MAD")
+
+
+
+
+function findMenuItem(list,ItemName) {
+  for ( let i = 0; i <list.length;i++);
+    if ( list.name===itemName)
+{ 
+        return list [i] ; 
+    }
+  } 
+     return null ; 
+
+  let menu = [
+    { name: "Espresso", price: 12 },
+    { name: "Cappuccino", price: 18 },
+    { name: "Croissant", price: 10 }
+]
+     console.log(findMenuitem(menu,(Cappuccino))) 
     
     
