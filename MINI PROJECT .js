73175f673f60b6954b9bbdbdf35876e69fd44054
fattatihafssa <fattatihafssa@gmail.com>
2026-09-24 09:47:26 +0000

    tt

---
+++ MINI PROJECT .js
@@ -0,0 +1,11 @@
+let menu = [
+     { name:"Espresso",price:12 , Category:"Coffee" },
+  { name:"cappuccino",price : 18 ,Category:"Coffee" },
+{ name:"Croissant",price:10 , category:"Coffee"},
+]
+
+  for(let i = 0 ; i <menu.length ; i++)
+ 
+    console.log(menu[i].name  ,  menu [i].price ,"MAD")
+    
+    
